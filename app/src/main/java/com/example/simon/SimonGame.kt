03bba8a8c31c87dfd7ce2bbd83f101943a4fbf1a
felix  bouet <felixbouet132@mail.com>
2024-02-06 12:15:25 +0100

    game finit

---
+++ app/src/main/java/com/example/simon/SimonGame.kt
@@ -0,0 +1,145 @@
+package com.example.simon
+
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class SimonGame(private val lifecycleScope: LifecycleCoroutineScope,
+                private val bindingClass: Class<*>,
+                private val textView: TextView,
+                private val fragment: Fragment,
+                private val view: View,
+                private val btnIds: List<Int>) {
+
+    private var score = 0
+
+    fun playGame() {
+        val sequenceClient: MutableList<Button> = mutableListOf()
+        val btns = mutableListOf<Button>()
+
+        btnIds.forEach { btnId ->
+            val btn = view.findViewById<Button>(btnId)
+            btn.setBackgroundResource(R.drawable.btn_normal)
+            btn.setOnClickListener {
+                addSelfButtonToSequenceClient(sequenceClient, btn)
+            }
+            btns.add(btn)
+        }
+
+        val sequenceGame = mutableListOf(getRandomBtn(btns))
+
+        updateScoreText("SCORE : 0")
+        lightOnOffBtn(sequenceGame)
+        startCheckingSequence(sequenceGame, sequenceClient, btns)
+    }
+
+
+    private fun getBindingInstance(view: View): Any {
+        val method = bindingClass.getDeclaredMethod("bind", View::class.java)
+        return method.invoke(null, view)
+    }
+    private fun updateScoreText(text: String) {
+        textView.text = text
+    }
+
+    private fun lightOnBtn(button: Button) {
+        button.setBackgroundResource(R.drawable.btn_to_click)
+    }
+
+    private fun lightOffBtn(button: Button) {
+        button.setBackgroundResource(R.drawable.btn_normal)
+    }
+
+    private fun lightOnOffBtn(buttonList: MutableList<Button>) {
+        lifecycleScope.launch {
+            for (button in buttonList) {
+                lightOnBtn(button)
+                delay(1000) // Attendre 1 seconde
+                lightOffBtn(button)
+            }
+        }
+    }
+
+    private fun clearSequence(buttonList: MutableList<Button>) {
+        buttonList.clear()
+    }
+
+    private fun addSelfButtonToSequenceClient(sequenceClient: MutableList<Button>, button: Button) {
+        sequenceClient.add(button)
+        val sequenceText = sequenceClient.joinToString(", ") { it.id.toString() }
+        Log.d("SequenceClient", "${button.id} ajouté à la séquence : $sequenceText")
+    }
+
+    private fun getRandomBtn(btns: List<Button>): Button {
+        val randomIndex = Random.nextInt(btns.size)
+        return btns[randomIndex]
+    }
+
+
+    private fun addRandomBtnTosequence(buttonList: MutableList<Button>, buttons: List<Button>) {
+        var newButton: Button
+        do {
+            newButton = getRandomBtn(buttons)
+        } while (newButton == buttonList.lastOrNull())
+
+        buttonList.add(newButton)
+
+        val sequenceText = buttonList.joinToString(", ") { it.id.toString() }
+        Log.d("SequenceClient", "${newButton.id} ajouté à la séquence : $sequenceText")
+    }
+
+
+    private fun startCheckingSequence(sequenceGame: MutableList<Button>, sequenceClient: MutableList<Button>, buttons: List<Button>) {
+        lifecycleScope.launch {
+            while (true) {
+                delay(1000) // Vérifier la séquence chaque seconde (ajustez selon vos besoins)
+
+                val result = checkSequence(sequenceGame, sequenceClient, buttons)
+                if (!result) {
+                    fragment.findNavController().navigate(R.id.resultFragment)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun checkSequence(sequenceGame: MutableList<Button>, sequenceClient: MutableList<Button>, buttons: List<Button>): Boolean {
+        val minSize = minOf(sequenceGame.size, sequenceClient.size)
+
+        for (i in 0 until minSize) {
+            val buttonGame = sequenceGame[i]
+            val buttonClient = sequenceClient[i]
+
+            if (buttonGame != buttonClient) {
+                // Réinitialiser les séquences
+                return false
+            }
+        }
+
+        // success
+        if (sequenceGame.size == sequenceClient.size) {
+            // La séquence client correspond entièrement à la séquence du jeu
+            score = incrementScore(score)
+            updateScoreText("SCORE : ${score.toString()}")
+
+            // Réinitialiser la séquence client
+            clearSequence(sequenceClient)
+            // Ajouter un nouveau bouton à la séquence du jeu
+            addRandomBtnTosequence(sequenceGame, buttons)
+            // Continuer le jeu
+            lightOnOffBtn(sequenceGame)
+        }
+        return true
+    }
+
+    private fun incrementScore(score: Int): Int {
+        return score + 1
+    }
+}
--- app/src/main/java/com/example/simon/fragments/EasyModeFragment.kt
+++ app/src/main/java/com/example/simon/fragments/EasyModeFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.simon.R
+import com.example.simon.SimonGame
 import com.example.simon.databinding.FragmentEasyModeBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -35,134 +37,20 @@ class EasyModeFragment : Fragment() {
 
         val homeButton = view.findViewById<Button>(R.id.homeBtn)
         homeButton.setOnClickListener {
-            findNavController().navigate(R.id.action_EasyMode_to_homeFragment2)
+            findNavController().navigate(R.id.homeFragment2)
         }
+
+        val lifecycleScope = viewLifecycleOwner.lifecycleScope
 
         val btnIds = listOf(R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4)
 
-        playGame(view, btnIds)
-    }
-
-    fun playGame(view: View, btnIds: List<Int>) {
-        val sequenceClient: MutableList<Button> = mutableListOf()
-        val btns = mutableListOf<Button>()
-
-        btnIds.forEach { btnId ->
-            val btn = view.findViewById<Button>(btnId)
-            btn.setBackgroundResource(R.drawable.btn_normal)
-            btn.setOnClickListener {
-                addSelfButtonToSequenceClient(sequenceClient, btn)
-            }
-            btns.add(btn)
-        }
-
-        val sequenceGame = mutableListOf(getRandomBtn(btns))
-        updateTestText("SCORE : 0")
-        lightOnOffBtn(sequenceGame)
-        startCheckingSequence(sequenceGame, sequenceClient, btns)
-    }
-
-
-    private fun updateTestText(text: String) {
-        binding.testText.text = text
-    }
-
-    private fun lightOnBtn(button: Button) {
-        button.setBackgroundResource(R.drawable.btn_to_click)
-    }
-
-    private fun lightOffBtn(button: Button) {
-        button.setBackgroundResource(R.drawable.btn_normal)
-    }
-
-    private fun lightOnOffBtn(buttonList: MutableList<Button>) {
-        lifecycleScope.launch {
-            for (button in buttonList) {
-                lightOnBtn(button)
-                delay(1000) // Attendre 1 seconde
-                lightOffBtn(button)
-            }
-        }
-    }
-
-    private fun clearSequence(buttonList: MutableList<Button>) {
-        buttonList.clear()
-    }
-
-    private fun addSelfButtonToSequenceClient(sequenceClient: MutableList<Button>, button: Button) {
-        sequenceClient.add(button)
-        val sequenceText = sequenceClient.joinToString(", ") { it.id.toString() }
-        Log.d("SequenceClient", "${button.id} ajouté à la séquence : $sequenceText")
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun getRandomBtn(btns: List<Button>): Button {
-        val randomIndex = Random.nextInt(btns.size)
-        return btns[randomIndex]
-    }
-
-
-    private fun addRandomBtnTosequence(buttonList: MutableList<Button>, buttons: List<Button>) {
-        var newButton: Button
-        do {
-            newButton = getRandomBtn(buttons)
-        } while (newButton == buttonList.lastOrNull())
-
-        buttonList.add(newButton)
-
-        val sequenceText = buttonList.joinToString(", ") { it.id.toString() }
-        Log.d("SequenceClient", "${newButton.id} ajouté à la séquence : $sequenceText")
-    }
-
-
-    private fun startCheckingSequence(sequenceGame: MutableList<Button>, sequenceClient: MutableList<Button>, buttons: List<Button>) {
-        lifecycleScope.launch {
-            while (true) {
-                delay(1000) // Vérifier la séquence chaque seconde (ajustez selon vos besoins)
-
-                val result = checkSequence(sequenceGame, sequenceClient, buttons)
-                if (!result) {
-                    findNavController().navigate(R.id.action_EasyMode_to_resultFragment)
-                    break
-                }
-            }
-        }
-    }
-
-    private fun checkSequence(sequenceGame: MutableList<Button>, sequenceClient: MutableList<Button>, buttons: List<Button>): Boolean {
-        val minSize = minOf(sequenceGame.size, sequenceClient.size)
-
-        for (i in 0 until minSize) {
-            val buttonGame = sequenceGame[i]
-            val buttonClient = sequenceClient[i]
-
-            if (buttonGame != buttonClient) {
-                // Réinitialiser les séquences
-                return false
-            }
-        }
-
-        // success
-        if (sequenceGame.size == sequenceClient.size) {
-            // La séquence client correspond entièrement à la séquence du jeu
-            score = incrementScore(score)
-            updateTestText("SCORE : ${score.toString()}")
-
-            // Réinitialiser la séquence client
-            clearSequence(sequenceClient)
-            // Ajouter un nouveau bouton à la séquence du jeu
-            addRandomBtnTosequence(sequenceGame, buttons)
-            // Continuer le jeu
-            lightOnOffBtn(sequenceGame)
-        }
-        return true
-    }
-
-    private fun incrementScore(score: Int): Int {
-        return score + 1
+        val scoreText = view.findViewById<TextView>(R.id.scoreText)
+        val simonGame = SimonGame(lifecycleScope,
+            this::class.java,
+            scoreText,
+            this,
+            view,btnIds)
+        simonGame.playGame()
     }
 
 }
