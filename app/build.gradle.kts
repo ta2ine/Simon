5e8e459d80bdb3add5d9992e21e738bff0adf1dd
felix  bouet <felixbouet132@mail.com>
2024-02-08 10:46:28 +0100

    ajout de firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt") // Plugin utilisé avec le databinding
+    id("kotlin-kapt")
+    id("com.google.gms.google-services") // Plugin utilisé avec le databinding
 }
 
 android {
@@ -54,10 +55,15 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
 
 
 
-    testImplementation("junit:junit:4.14-SNAPSHOT")
+    testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    //implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    //implementation("com.google.firebase:firebase-analytics")
 }
